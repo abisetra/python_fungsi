47173536dc322cd6ce55b947022f130096dbe48d
abisetra <abiyyusetrayana63@gmail.com>
2022-05-27 15:46:35 +0700

    Initial release

---
+++ fungsi_parameter.py
@@ -0,0 +1,7 @@
+def greetings(nama, waktu):
+    print('hello', nama, 'selamat', waktu)
+    print('apakah', nama, 'sudah datang', waktu, 'ini?')
+
+greetings('andi', 'pagi')
+greetings('rudi', 'siang')
+greetings('wahyu', 'malam')
