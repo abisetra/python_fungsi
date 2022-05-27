1bbece78efb8a284110529fd50aa19853fe26b60
abisetra <abiyyusetrayana63@gmail.com>
2022-05-27 16:00:46 +0700

    Penambahan Fungsi Parameter

--- fungsi_parameter.py
+++ fungsi_parameter.py
@@ -1,5 +1,16 @@
 def greetings(nama, waktu):
     print('hello', nama, 'selamat', waktu)
 
-greetings('malam', 'wahyu') #Positional parameter
-greetings(waktu='malam', nama='wahyu')  #named parameter
+# greetings('malam', 'wahyu') #Positional parameter
+# greetings(waktu='malam', nama='wahyu')  #named parameter
+
+def penjumlahan(a,b):
+    hasil = a+b
+    return hasil
+
+data_greeting = greetings('wahyu', 'pagi')
+
+hasil_jumlah = penjumlahan(2,5)
+
+print(data_greeting)
+print(hasil_jumlah)
